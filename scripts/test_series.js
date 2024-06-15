c9d7bb91750e3c93c04f99776527d818138a8e0f
PranitAdgokar <136226664+PranitAdgokar@users.noreply.github.com>
2024-06-15 17:34:43 +0530

    chore: Update npm dependencies and add @motionone/utils@10.18.0

---
+++ scripts/test_series.js
@@ -0,0 +1,48 @@
+import { time } from '@motionone/utils'
+
+const TestSeriesData = [
+    {
+        subject:
+            'Indian Polity + Governance + Current Issues of Polity (From 1st JAN 2023 to 31st DEC 2023)',
+        sources: [
+            "Chanakya's Class Notes",
+            'Tamilnadu state Board Textbook.',
+            'NCERT - 11th Std',
+            'Indian Constitution at Work',
+            'M.Laxikant',
+            'For current Affairs:The Hindu(TH),Indian Express(IE),Loksatta,PIB,ETC.',
+        ],
+        date: '21-01-2024 SUNDAY',
+        time: '09:30 AM to 11:30 AM',
+    },
+    {
+        subject:
+            'Physical Geography (Indian + World) + Current Affairs of Geography (From 1st JAN 2023 to 31st DEC 2023)',
+        sources: [
+            "Chanakya's Class Notes",
+            'NCERT - 11th Std(Social Science - Our Environment)',
+            'NCERT - 11th Class(Both-Fundamentals of Physical Geography & India: Physical Environment)',
+            'G.C.Leong',
+            'Oxford Atlas',
+            'TH;IE;LOKSATTA;PIB; Down To Earth ETC.',
+        ],
+        date: '28-01-2024 SUNDAY',
+    },
+    {
+        subject: 'CSAT',
+        sources: 'Based on previous year question papers',
+        date: '28-01-2024 SUNDAY',
+    },
+    {
+        subject:
+            'History Part-I: Ancient + Medical + Art + Culture + Current Affairs Of Culture, From 1st JAN 2023 to 15th MARCH 2024',
+        sources: [
+            'Tamilnadu Textbook 12th class Or',
+            '11th Class NCERTS',
+            'Themes (Only Boxes & Summary From New NCERTS)',
+            'TH;IE;LOKSATTA;PIB;ETC.',
+        ],
+        date: '24-03-2024 SUNDAY',
+        time: '09:30 AM to 11:30 AM',
+    },
+]
